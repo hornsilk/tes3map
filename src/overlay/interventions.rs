5750d75029d0b1892f5ee578414177c7d860b8af
hornsilk <hornsilk01@gmail.com>
2024-12-20 16:26:06 -0800

    v0.9 - correctly calculate bounding box

--- src/overlay/interventions.rs
+++ src/overlay/interventions.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap};
 
 use eframe::epaint::Stroke;
 use egui::{emath::RectTransform, Color32, Shape};
@@ -7,76 +7,59 @@ use tes3::esp::Cell;
 use crate::{dimensions::Dimensions, get_center_from_cell, get_tri_at_cell, CellKey};
 use voronoice::*;
 use egui::Pos2;
-use std::cmp::max;
 
 
-pub fn convert_from_voronoi(
-    p: &Point,
-    dimensions: &Dimensions, 
-) -> Pos2 {
-   
-    let new_x = p.x as i32 - dimensions.min_x;
-    let new_y = -p.y as i32 + dimensions.max_y;
-    let new_center = Pos2{x:new_x as f32, y:new_y as f32};
 
-    new_center
-
-}
 
 pub fn create_voronoi_polygons(
     to_screen: RectTransform,
     dimensions: &Dimensions,
     interventions: &HashMap<CellKey, Cell>,
 ) -> Vec<Shape> {
-    // let dx = dimensions.max_x - dimensions.min_x;
-    let dx = (to_screen.to().max.x - to_screen.to().min.x) as i32;
-    // let dy = dimensions.max_y - dimensions.min_y;
-    let dy = (to_screen.to().max.y - to_screen.to().min.y) as i32;
-    // let bounding_size = max(dx, dy) as f64 * 1.2;
-    let bounding_size = 2000.0;
+    let dx = (to_screen.to().max.x - to_screen.to().min.x) as f64;
+    let cx = ((to_screen.to().max.x + to_screen.to().min.x)/2.0) as f64;
+    let dy = (to_screen.to().max.y - to_screen.to().min.y) as f64;
+    let cy = ((to_screen.to().max.y + to_screen.to().min.y)/2.0) as f64;
+    let bounding_box = BoundingBox::new( Point{x:cx,y:cy}, dx, dy);
 
     let n_points = interventions.keys().len();
     let mut centers: Vec<Point> = Vec::with_capacity(n_points as usize);
     let mut shapes: Vec<Shape> = Vec::new();
 
     for key in interventions.keys() {
-        let old_pt = Point{x: (key.0 as f64) , y: (key.1 as f64) };
         let temp = get_center_from_cell(dimensions, to_screen, key.clone());
-        let new_pt = Point{x: (temp.x as f64) , y: (temp.y as f64) };
-
-        println!("({},{}) --> ({},{})", old_pt.x,old_pt.y, new_pt.x, new_pt.y);
         centers.push( Point{x: (temp.x as f64) , y: (temp.y as f64) });
     }
+
     let my_voronoi = VoronoiBuilder::default()
         .set_sites(centers)
-        .set_bounding_box(BoundingBox::new_centered_square(bounding_size)) 
+        .set_bounding_box(bounding_box) 
         .build()
         .unwrap();
 
 
     for my_cell in my_voronoi.iter_cells() {
-        let my_verts = my_cell.iter_vertices().collect::<Vec<&Point>>();
-        let n_vertecies = my_verts.len();
-        let mut my_verticies: Vec<Pos2> = Vec::with_capacity(n_vertecies as usize);
+        let verts = my_cell.iter_vertices().collect::<Vec<&Point>>();
+        let n_vertecies = verts.len();
+        let mut verts_pos2: Vec<Pos2> = Vec::with_capacity(n_vertecies as usize);
     
-        for vert in my_verts {
-            let my_vert = Pos2 {x: vert.x as f32, y:vert.y as f32};
-            my_verticies.push(my_vert);
+        for vert in verts {
+            verts_pos2.push(Pos2 {x: vert.x as f32, y:vert.y as f32});
         }
+
+
         let color = Color32::from_rgb(0, 255, 0);
         let color2 = Color32::from_rgb(0, 255, 0);
         let color2 = color2.gamma_multiply(0.1);
-        let my_polygon = Shape::convex_polygon(my_verticies, color2, Stroke::new(3.0, color));
+        let polygon = Shape::convex_polygon(verts_pos2, color2, Stroke::new(3.0, color));
+        
+        shapes.push(polygon);
         
         
-        shapes.push(my_polygon);
-        
-        
-        let my_new_center = Pos2 {x: my_cell.site_position().x as f32, y:my_cell.site_position().y as f32};
-
-        let radius = 10.0;
-        let my_dot = Shape::circle_filled(my_new_center, radius, color);
-        shapes.push(my_dot);
+        // let my_new_center = Pos2 {x: my_cell.site_position().x as f32, y:my_cell.site_position().y as f32};
+        // let radius = 10.0;
+        // let my_dot = Shape::circle_filled(my_new_center, radius, color);
+        // shapes.push(my_dot);
     }
 
     shapes
